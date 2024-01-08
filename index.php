2acb42030e776c52b2ce448877c78ae97ef28f3e
antoniodalterio <antoniodalterio2227@gmail.com>
2024-01-08 17:45:06 +0100

    feat: style

--- index.php
+++ index.php
@@ -4,13 +4,30 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Badwords</title>
+  <!-- Googlw Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Pridi:wght@400;700&display=swap" rel="stylesheet">
+  <!-- Googlw Fonts -->
+
+  <!-- MY css -->
+  <link rel="stylesheet" href="style.css">
+  <!-- MY css -->
+
 </head>
 <body>
-  <form action="pag2.php" method="get">
-    <textarea name="name" id="name" cols="30" rows="10"></textarea>
 
-    <label for="word">Inserisci parola da censurare</label>
-    <input type="text" name='word' id='word'>
+  <form action="pag2.php" method="get" class='glass'>
+    <div class='input-area'>
+      <label for="name">Scrivi una frase</label>
+      <textarea name="name" id="name" cols="30" rows="10"></textarea>
+    </div>
+   
+    <div class='input-area'> 
+      <label for="word">Inserisci parola da censurare</label>
+      <input type="text" name='word' id='word'>
+    </div>
+   
     <button>invia</button>
   </form>
 </body>
--- pag2.php
+++ pag2.php
@@ -1,16 +1,63 @@
 <?php 
- 
-$text= strtolower($_GET['name']);
+  #chiamata Get di 'name'
+  $text= strtolower($_GET['name']);
 
-$bad_word= strtolower($_GET['word']);
+  #chiamata Get di 'word'
+  $bad_word= strtolower($_GET['word']);
 
-echo $text;
-
-echo strlen($text);
-
-
-$text_censored= str_replace($bad_word, '***', $text);
-
-echo $text_censored.strlen($text_censored);
+  #sostituzine parola censurata
+  $text_censored= str_replace($bad_word, '***', $text);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bad Words</title>
+
+  <!-- Googlw Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Pridi:wght@400;700&display=swap" rel="stylesheet">
+  <!-- Googlw Fonts -->
+
+  <!-- my css -->
+  <link rel="stylesheet" href="style.css">
+  <!-- my css -->
+
+</head>
+<body>
+  <!-- div contenente la frase senza censura -->
+  <div class='glass container'>
+    <h2>Frase senza la parola censurata</h2>
+    <p>
+      <?php echo ucfirst($text); ?>
+    </p>
+
+    <p>
+      la frase ha una lunghezza di, 
+      <?php echo strlen($text); ?>,
+      caratteri.
+    </p>
+
+  </div>
+
+  <!-- div contenente la frase con censura -->
+  <div class='glass container'>
+    <h2>Frase con la parola censurata</h2>
+    <p>
+     <?php echo ucfirst($text_censored); ?>
+    </p>
+
+    <p>
+      La frase ha una lunghezza di, 
+      <?php echo strlen($text_censored); ?>,
+      caratteri.
+    </p>
+
+  </div>
+
+</body>
+</html>
